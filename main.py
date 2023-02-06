e64d89358e2911e2116e6a0edd3072ff58876b5f
Jean-Olivier Pitre <ojpitre@ojpitre.ai>
2023-02-06 09:50:00 -0500

    Removing the year 2024 from the curriculum filename

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from docx2pdf import convert
 
 # Converting docx specifying both the input and output paths
-convert("CV-JeanOlivierPitre-2024.docx", "CV-JeanOlivierPitre-2024.pdf")
+convert("CV-JeanOlivierPitre.docx", "CV-JeanOlivierPitre.pdf")
